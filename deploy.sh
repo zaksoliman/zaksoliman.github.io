e6099fb3f788df7d5e9ef4b726ac1efdf60c20ce
Zack Soliman <zack.soliman@gmail.com>
2020-04-14 18:36:08 -0400

    Fixed CNAME in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ghp-import --cname=www.zaksolman.com output -r origin -b master
+ghp-import --cname=www.zaksoliman.com output -r origin -b master
 git push origin master
 git checkout source 
